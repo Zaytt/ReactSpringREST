ee26b5d47d69ee0767aee0b63d69a334020cb068
Zaytt <ivan_chavez_e@hotmail.com>
2018-06-16 14:12:21 -0500

    First Commit

---
+++ src/main/java/com/example/javareact/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.javareact.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
